5099ca88103ccdba38574d950436c814ba516be5
Huang Jian-Min <yfr.huang@hotmail.com>
2020-03-21 15:52:44 +0800

    feat: Init Commit

---
+++ src/main/kotlin/com/example/kotlinspringdemo/KotlinSpringDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringdemo/KotlinSpringDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
